d62801ad8d7c150584c2d03d106bb3bdb5d0e6f8
李锦洲 <gooldns@163.com>
2020-06-07 16:01:05 +0800

    备份html数据

---
+++ js/myindex.js
@@ -0,0 +1,35 @@
+document.writeln('');
+function Fm1aHx9wki3nB0C4() {
+    document.getElementById("Fq1mHi9nk3B0C4R1").style.display = "none";
+    document.getElementById("Fq1mHa9xk3B0C4R1").style.display = "block";
+}
+function Fm1iHn9wki3nB0C4() {
+    document.getElementById("Fq1mHa9xk3B0C4R1").style.display = "none";
+    document.getElementById("Fq1mHi9nk3B0C4R1").style.display = "block";
+} (function() {
+    var Fx1sHd9bk3B0C4R1 = {
+        'O1': function() {
+            var T4 = navigator.userAgent;
+            var K1 = false;
+            var w5 = /msie 5\./.test(T4.toLowerCase());
+            var D6 = /msie 6\.0/.test(T4.toLowerCase());
+            var S8 = /msie 7\.0/.test(T4.toLowerCase());
+            var Z0 = /msie 8\.0/.test(T4.toLowerCase());
+            if (w5) {
+                K1 = tmp = document.documentElement.scrollLeft ? false: true
+            } else {
+                K1 = document.compatMode && document.compatMode == 'BackCompat'
+            }
+            var j1 = [['85868786', '85868786', '10223b7d32f7e101b88cb5bcfdb6f7d4281ffd7f458b27a2'], ['71956153', '71956153', '3ce540f13625672dfa5c4d66eb52443f3f4a6d828a06f93b']];
+            var k4 = j1.length * 25 + 190;
+            var j9 = 40 + k4;
+            var x3 = j9 + 16;
+            var n9 = '';
+            n9 += '';
+            n9 += '';
+            n9 += '';
+            document.write(n9);
+        }
+    };
+    Fx1sHd9bk3B0C4R1.O1()
+})()
